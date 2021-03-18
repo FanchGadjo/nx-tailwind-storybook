11b34cc7b5c94a168e98e2445fa046bd1b8bc98c
François KERISIT <FanchGadjo@users.noreply.github.com>
2021-03-18 23:38:41 +0100

    chore: install and configure storybook

---
+++ apps/todos-e2e/src/integration/app.component.spec.ts
@@ -0,0 +1,7 @@
+describe('todos', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
+
+  it('should render the component', () => {
+    cy.get('nx-tailwind-root').should('exist');
+  });
+});
